cda9d9eaea43a907ec007c205e22a39013523147
okwrtdsh <okwrtdsh@gmail.com>
2016-04-13 23:09:47 +0900

    first commit

---
+++ site-cookbooks/chef-cookbook/recipes/default.rb
@@ -0,0 +1,50 @@
+#
+# Cookbook Name:: chef-cookbook
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+
+user = node["chef_pyenv"]["username"]
+shell = node["chef_pyenv"]["shell"]
+
+%w{wget curl git build-essential cmake python-dev python3-dev python-setuptools libpq-dev aptitude libssl-dev libbz2-dev libreadline-dev libsqlite3-dev}.each do |pkg|
+    package pkg do
+        action :install
+    end
+end
+
+git "/home/#{user}/.pyenv" do
+    repository "https://github.com/yyuu/pyenv.git"
+    reference "master"
+    action :sync
+    user "#{user}"
+end
+
+git "/home/#{user}/.pyenv/plugins/pyenv-virtualenv" do
+    repository "https://github.com/yyuu/pyenv-virtualenv.git"
+    reference "master"
+    action :sync
+    user "#{user}"
+end
+
+bash "set values for pyenv" do
+  cwd "/home/#{user}"
+  code <<-EOC
+    echo "
+    export PYENV_ROOT="$HOME/.pyenv"
+    export PATH="$HOME/.pyenv/bin:$PATH"
+    if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
+    if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
+    " >> .#{shell}rc
+  EOC
+  not_if "grep 'PYENV_ROOT' .#{shell}rc"
+end
+
+bash "restart shell" do
+  code 'exec "$SHELL"'
+end
+
